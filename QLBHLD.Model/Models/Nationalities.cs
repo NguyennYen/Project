33b2f770520070f676074258125f9c5dc7f2a824
NguyennYen <yenle210399@gmail.com>
2021-08-04 17:19:47 +0700

    second commit

---
+++ QLBHLD.Model/Models/Nationalities.cs
@@ -0,0 +1,31 @@
+﻿using QLBHLD.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBHLD.Model
+{
+    [Table("Provinces")]
+    public class Nationalities : Auditable
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string code { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string nation_name { get; set; }
+
+        [StringLength(1023)]
+        public string remark { get; set; }
+         
+        public virtual IEnumerable<Province> Provinces { get; set; }
+    }
+}
