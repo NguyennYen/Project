7302ea0dbbca8ba6f82ef785646db0d05030729b
VietDuc0908 <86755488+VietDuc0908@users.noreply.github.com>
2021-08-05 19:10:42 +0700

    Duc-Model 1-14

---
+++ QLBHLD.Model/Models/hu_title.cs
@@ -0,0 +1,28 @@
+﻿using QLBHLD.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBHLD.Model.Models
+{
+    [Table("hu_title")]
+    public class hu_title : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        //Mã chức danh
+        [StringLength(50)]
+        public string code { get; set; }
+
+
+        //Tên chức danh
+        [StringLength(255)]
+        public string title_name { get; set; }
+    }
+}
