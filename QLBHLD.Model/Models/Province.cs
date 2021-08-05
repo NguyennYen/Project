87d44cecc7d8b466922f2d61859af503945dcd9c
Ngo Duc Thin <thinnd@vmodev.com>
2021-08-05 18:41:16 +0700

    Revert "add model 15-28"
    This reverts commit e58efb579b1068175c535d814ff79fb14b051eb0.

---
+++ QLBHLD.Model/Models/Province.cs
@@ -0,0 +1,37 @@
+﻿using QLBHLD.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBHLD.Model
+{
+    [Table("Provinces")]
+    public class Province : Auditable
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string code { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string province_name { get; set; }
+
+        [Required]
+        public int nation_id { get; set; }
+
+        [ForeignKey("nation_id")]
+        public virtual Nationalities Nationalities { get; set; }
+
+        [StringLength(1023)]
+        public string remark { get; set; }
+        
+        public virtual IEnumerable<Districts> Districts { get; set; }
+    }
+}
---
+++ QLBHLD.Model/Models/Wards.cs
@@ -0,0 +1,35 @@
+﻿using QLBHLD.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBHLD.Model
+{
+    [Table("Wards")]
+    public class Wards : Auditable
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string code { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string wards_name { get; set; }
+
+        [Required]
+        public int district_id { get; set; }
+
+        [ForeignKey("district_id")]
+        public virtual Districts Districts{ get;set;}
+
+        [StringLength(1023)]
+        public string remark { get; set; }
+    }
+}
